cba240fccd61ff2c1915b8a1d20f0fc261819e0c
thisisaaronland <whosonfirst@mapzen.com>
2016-08-21 22:32:25 +0000

    make a png file! still unclear whether all the tmp file stuff is necessary...

--- src/main/java/com/mapzen/whosonfirst/squeegee/BatikResource.java
+++ src/main/java/com/mapzen/whosonfirst/squeegee/BatikResource.java
@@ -1,6 +1,8 @@
 package com.mapzen.whosonfirst.squeegee;
 
 // https://xmlgraphics.apache.org/batik/using/transcoder.html
+// https://xmlgraphics.apache.org/batik/javadoc/org/apache/batik/transcoder/TranscoderInput.html
+// https://xmlgraphics.apache.org/batik/javadoc/org/apache/batik/transcoder/TranscoderOutput.html
 
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.apache.batik.transcoder.TranscoderInput;
@@ -9,6 +11,10 @@ import org.apache.batik.transcoder.TranscoderOutput;
 import java.io.InputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ByteArrayInputStream;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
@@ -19,9 +25,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
-
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.Response;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,8 +65,29 @@ public class BatikResource {
 	
 	PNGTranscoder t = new PNGTranscoder();	    
 	TranscoderInput input = new TranscoderInput(svgStream);
+
+	File tmpfile;
+	FileOutputStream ostream;
 	
-	ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+	try {
+	    tmpfile = File.createTempFile("svg", ".png");
+	}
+	
+	catch (Exception e){
+	    LOGGER.error("Failed to create tmp file, because " + e.toString());
+	    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
+	}
+
+	try {
+	    ostream = new FileOutputStream(tmpfile.toString());
+	}
+
+	catch (Exception e){
+	    LOGGER.error("Failed to create output stream, because " + e.toString());
+	    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
+	}
+	    
+	// ByteArrayOutputStream ostream = new ByteArrayOutputStream();
 	TranscoderOutput output = new TranscoderOutput(ostream);
 
 	try {
@@ -78,8 +107,43 @@ public class BatikResource {
 	catch (Exception e){
 	  return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
 	}
+
+	InputStream im;
+	BufferedImage buf;
 	
-	return Response.status(Response.Status.OK).entity(output).build();
+	try {
+	    im = new FileInputStream(tmpfile.toString());
+	}
+
+	catch (Exception e){
+	    LOGGER.error("Failed to create input stream, because " + e.toString());
+	    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
+	}
+
+	try {
+	    buf = ImageIO.read(im);
+	}
+
+	catch (Exception e){
+	    LOGGER.error("Failed to read input stream, because " + e.toString());
+	    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
+	}
+	
+	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+	try {
+	    ImageIO.write(buf, "png", bos);
+	}
+
+	catch (Exception e){
+	    LOGGER.error("Failed to write image buffer, because " + e.toString());
+	    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
+	}
+
+	
+	byte[] imageInBytes = bos.toByteArray();
+	
+	return Response.status(Response.Status.OK).entity(imageInBytes).build();
     }
     
 }
